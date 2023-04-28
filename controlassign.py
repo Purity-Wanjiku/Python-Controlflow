3fdca651482e169e86443d5fc3be0664754295f7
Purityndirangu <ndiranguwanjikupurity@gmail.com>
2023-04-28 23:43:42 +0300

    Done control flow questions

---
+++ controlassign.py
@@ -0,0 +1,33 @@
+#Write a function that uses while, if and continue statements to
+# print all the even numbers between 0 and 50. 
+def even_numbers():
+    z = 0
+    while z < 50:
+        z += 1
+        if z%2 != 0:
+            continue
+        print (z)
+       
+#Write a function that takes an integer argument and 
+# prints "Prime" if the number is prime, and "Not prime" if the number is not prime.
+def prime_numbers(integer):
+      if integer < 2:
+        print("Not Prime")
+      i = 2
+      while i <= integer / 2:
+       if integer % i == 0:
+        print("Not Prime") 
+        i += 1
+        print("Prime")
+                 
+#Write a function that takes a list of integers as input 
+# and prints the sum of all the even numbers in the list.
+
+#Write a function that takes any two integers as input,
+# and prints the sum of all the numbers between the two integers (inclusive) 
+# that are divisible by 3.                 
+            
+        
+       
+       
+        
